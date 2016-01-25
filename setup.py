3d729e1349230b138aeb0a833cd56a87d92d1320
hkff <infigmo@gmail.com>
2016-01-25 18:41:36 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fodtlmon',
+    version='1.0',
+    packages=['parser', 'dtl', 'fodtl', 'fotl', 'ltl', 'tools'],
+    url='https://github.com/hkff/fodtlmon',
+    license='GPL3',
+    author='Walid Benghabrit',
+    author_email='Walid.Benghabrit@mines-nantes.fr',
+    description='FODTLMON is a monitoring framework based on distributed first order linear temporal logic.'
+)
