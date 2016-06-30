62b11b9ed8877c08399ddcd04421d56eee6ae344
hkff <infigmo@gmail.com>
2016-06-30 14:33:10 +0200

    Complete Benchmarks

--- fodtlmon/tests/benchmark.py
+++ fodtlmon/tests/benchmark.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 __author__ = 'walid'
-
 from fodtlmon.fodtl.fodtlmon import *
 from datetime import datetime
 from fodtlmon.ltl.test import Fuzzer
@@ -67,24 +66,112 @@ class InstrumentedMon(Fotlmon):
         return ret
 
 
-def run_benchmarks():
-    alphabet = ["P"]
-    constants = ["a", "b", "c"]
+def run_formula(base_dir, f_dir):
+    """
+    Run benchmark on a formula
+    :param base_dir: base directory
+    :param f_dir: form FormulaX
+    :return:
+    """
+    print(f_dir)
+    formula_file = "%s/%s/%s.formula" % (base_dir, f_dir, f_dir.replace("Formula", ""))
+    trace_file = "%s/%s/%s.trace" % (base_dir, f_dir, f_dir.replace("Formula", ""))
+    with open(formula_file, "r") as f:
+        formula = f.read()
+    with open(trace_file, "r") as f:
+        trace = f.read()
+
+    tr = Trace().parse(trace)
+    fl = FodtlParser.parse(formula)
+
+    # 1. No optimization
+    output_file = "%s/%s/gen.dat" % (base_dir, f_dir)
+    InstrumentedMon(fl, tr).monitor(optimization=Optimzation.NONE, output=output_file)
+
+    # 2. Simplification
+    output_file = "%s/%s/gen_simplification.dat" % (base_dir, f_dir)
+    InstrumentedMon(fl, tr).monitor(optimization=Optimzation.SIMPLIFICATION, output=output_file)
+
+    # 3. Solver
+    output_file = "%s/%s/gen_solver.dat" % (base_dir, f_dir)
+    InstrumentedMon(fl, tr).monitor(optimization=Optimzation.SOLVER, output=output_file)
+
+    # 4. Fixpoint
+    output_file = "%s/%s/gen_fixpoint.dat" % (base_dir, f_dir)
+    InstrumentedMon(fl, tr).monitor(optimization=Optimzation.FIXPOINT, output=output_file)
+
+
+def run_benchmarks(bench_dir="fodtlmon/tests/benchmarks/", jobs=1):
+    """
+    Run benchmarks
+    """
+    # Get formulas folders
+    formulas_dir = []
+    dirs = os.listdir(bench_dir)
+    for file in dirs:
+        formulas_dir.append(file)
+
+    # Check for joblib
+    import importlib
+    parallel = importlib.find_loader("joblib") is not None
+
+    if parallel and jobs > 1:
+        # Parallel version
+        from joblib import Parallel, delayed
+        Parallel(n_jobs=jobs)(delayed(run_formula)(bench_dir, x) for x in formulas_dir)
+    else:
+        # Sequential version
+        for x in formulas_dir:
+            run_formula(bench_dir, x)
+
+
+def gen_formula_trace(x, fuzzer, formula_nbr, formula_depth, trace_length, trace_depth, write_dir="fodtlmon/tests/benchmarks/"):
+    """
+    Generate a formula and a trace
+    """
+    f_dir = "%sFormula%s" % (write_dir, x)
+    os.mkdir(f_dir)
+    formula = fuzzer.gen(formula_depth)
+    trace = fuzzer.gen_trace(trace_length, depth=trace_depth, preds=formula.walk(filter_type=P))
+    with open("%s/%s.formula" % (f_dir, x), "w+") as f: f.write(str(formula))
+    with open("%s/%s.trace" % (f_dir, x), "w+") as f: f.write(str(trace))
+
+
+def generate_benchmarks(formula_nbr=1, formula_depth=2, trace_length=5, trace_depth=1, jobs=1, alphabet=None, constants=None):
+    """
+    Generate formulas and traces for benchmarks
+    :param formula_nbr:
+    :param formula_depth:
+    :param trace_length:
+    :param trace_depth:
+    :param jobs:
+    :param alphabet:
+    :param constants:
+    :return:
+    """
+    alphabet = ["P"] if alphabet is None else alphabet
+    constants = ["a", "b", "c"] if constants is None else constants
     fuzzer = Fuzzer("fotl", alphabet=alphabet, constants=constants)
     fuzzer.init_fuzzer()
-    # GEN
-    # 1. Create benckmarks folder
-    # 2. Create a folder for each formula
-    # 3. Generate a trace for the formula
 
-    # RUN
-    # For each formula folder
-    #    trace = read from trace file
-    #    formual = read from formula file
-    #    tr = Trace().parse(trace)
-    #    fl = FodtlParser.parse(formula)
-    #
-    #    # Run in parallel (optimize = -1, 0, 1, 2, 3)
-    #    mon = InstrumentedMon(fl, tr)
-    #    res = mon.monitor(optimization=optimize, output=output_file)
-    #
+    import datetime
+    output_dir = "fodtlmon/tests/benchmarks_%s/" % (datetime.datetime.today().strftime("%Y-%m-%d_%H:%M:%S"))
+
+    os.mkdir(output_dir)
+
+    # Check for joblib
+    import importlib
+    parallel = importlib.find_loader("joblib") is not None
+
+    if parallel and jobs > 1:
+        # Parallel version
+        from joblib import Parallel, delayed
+        Parallel(n_jobs=jobs)(delayed(gen_formula_trace)(x, fuzzer, formula_nbr, formula_depth, trace_length,
+                                                         trace_depth, write_dir=output_dir)
+                              for x in range(formula_nbr))
+    else:
+        # Sequential version
+        for x in range(formula_nbr):
+            gen_formula_trace(x, fuzzer, formula_nbr, formula_depth, trace_length, trace_depth, write_dir=output_dir)
+
+    return output_dir
