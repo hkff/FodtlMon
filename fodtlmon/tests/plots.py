a8eaae63d774420ac2606c4c4bfe8fc41befe4f3
hkff <infigmo@gmail.com>
2016-07-01 10:33:34 +0200

    minors

--- fodtlmon/tests/plots.py
+++ fodtlmon/tests/plots.py
@@ -35,21 +35,21 @@ def load_formula_data(formula_dir, bench_dir):
     file = "%s/%s/gen_fixpoint.dat" % (bench_dir, formula_dir)
     data_fxp = pd.read_table(file, sep=' \s+', header=0,  encoding='utf-8', comment='#', skip_blank_lines=True, engine="python")
 
-    return {"data_none": data_none, "data_simp": data_simp, "data_solver": data_solver, "data_fxp": data_fxp}
+    return {"none": data_none, "simp": data_simp, "solver": data_solver, "fxp": data_fxp}
 
 
 def plot_formula_stats(formula_dir, bench_dir):
     data = load_formula_data(formula_dir, bench_dir)
 
     types = ["Formula-length", "Exec-time(ms)"]
-    for type in types:
+    for tp in types:
         plt.clf()
-        plt.plot(data["data_none"][type], label="None")
-        plt.plot(data["data_simp"][type], label="Simplification")
-        plt.plot(data["data_solver"][type], label="Solver")
-        plt.plot(data["data_fxp"][type], label="Fixpoint")
+        plt.plot(data["none"][tp], label="None")
+        plt.plot(data["simp"][tp], label="Simplification")
+        plt.plot(data["solver"][tp], label="Solver")
+        plt.plot(data["fxp"][tp], label="Fixpoint")
         legend = plt.legend(loc='upper left', shadow=True)
-        plt.savefig("%s/%s/plot_%s.png" % (bench_dir, formula_dir, type))
+        plt.savefig("%s/%s/plot_%s.png" % (bench_dir, formula_dir, tp))
         #plt.show()
 
 
@@ -57,7 +57,32 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Missing argument ! Usage: plots.py <bench_dir> <formula_nbr>")
         exit(-1)
+
     bench_dir = sys.argv[1]
     formula_dirs = ["Formula%s" % x for x in range(int(sys.argv[2]))]
+
+    # 1. Generate graph for each formula
+    #for formula_dir in formula_dirs:
+    #    plot_formula_stats(formula_dir, bench_dir)
+
+    # 2. Load all formulas data
+    formulas_data = []
     for formula_dir in formula_dirs:
-        plot_formula_stats(formula_dir, bench_dir)
+        formulas_data.append(load_formula_data(formula_dir, bench_dir))
+    #print(formulas_data)
+
+    plt.clf()
+    df = pd.Series({'None':           formulas_data[0]["none"]["Exec-time(ms)"].sum(0),
+                    'Simplification': formulas_data[0]["simp"]["Exec-time(ms)"].sum(0),
+                    'Solver':         formulas_data[0]["solver"]["Exec-time(ms)"].sum(0),
+                    'Fixpoint':       formulas_data[0]["fxp"]["Exec-time(ms)"].sum(0)})
+    df.plot(kind='bar')
+    #plt.show()
+
+    plt.clf()
+    df = pd.Series({'None':           formulas_data[0]["none"]["Formula-length"].sum(0),
+                    'Simplification': formulas_data[0]["simp"]["Formula-length"].sum(0),
+                    'Solver':         formulas_data[0]["solver"]["Formula-length"].sum(0),
+                    'Fixpoint':       formulas_data[0]["fxp"]["Formula-length"].sum(0)})
+    df.plot(kind='bar')
+    plt.show()
